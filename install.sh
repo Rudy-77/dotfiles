3946443127bc97fbba54886af4080e990eb9b949
Rudy-77 <pau876171@gmail.com>
2024-11-28 17:38:23 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget -O MADRID.jpg https://www.madrid-traveller.com/wp-content/uploadas/2024/08/real-madrid-club-de-futbol.jpg
+echo \
+cp /home/quetzal/Downloads/MADRID.jpg /home/quetzal/Pictures
+mv /home/quetzal/Downloads/OxProto.zip /users/share/fonts
+
+echo "Felicidades completaste el curso de linux noob a pro" > hola.txt
+echo "Guion completado"
