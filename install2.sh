8e956571ec26ae13d14b2747b3c81a48b8273825
Rudy-77 <pau876171@gmail.com>
2024-11-28 18:57:31 +0000

    Add files via upload

---
+++ install2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo snap install netbeans --classic
+sudo apt install dia
+sudo snap install code --classic
+sudo snap install intellij-idea-community --classic
+sudo snap install android-studio --classic
+
+sudo apt install php
+sudo apt install php8.2
+sudo apt install php8.2-mysql php8.2-xml php8.2-curl
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
+sudo apt install php8.1
